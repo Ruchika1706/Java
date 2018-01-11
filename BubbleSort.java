fb4840da44c4c48e635f999498fe5de83f6720fb
Ruchika <luthraruchika19@gmail.com>
2018-01-11 21:35:37 +0530

    Explanation of Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -10,7 +10,9 @@ class BubbleSort {
             System.out.println("Enter the element of array");
             arr[i] = si.nextInt();
         }
+	/* Outer Loop is for number of times to do this swap if greater, number of elements-1, last minimum element taken care itself*/
         for(int i = 0;i<(arr.length-1);i++){
+	/*This loop need to iterate till second last element, comparison with last element taken care here already*/
             for(int j=0;j<=(arr.length-2);j++){
                 if(arr[j]>arr[j+1]){
                     temp = arr[j+1];
