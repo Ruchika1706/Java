0e4fbe5734bd151e0dae2d3fddecc7570450cf15
Ruchika <luthraruchika19@gmail.com>
2018-02-14 14:51:51 +0530

    testing string and stringbuffer for equals function

---
+++ Testing123.java
@@ -0,0 +1,11 @@
+public class Testing123 {
+    public static void main(String[] args)
+    {
+        StringBuffer sb = new StringBuffer("javachamp");
+        String s = new String("javachamp");
+        boolean stmt1 = s.equals(sb) ;
+        System.out.println(stmt1);
+        System.out.println(null+"Hi");
+
+    }
+}
