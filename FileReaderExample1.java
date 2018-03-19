62ae9c3f5b3c61687cf2ab31a93005e2618bdd56
ruchika1706 <luthraruchika19@gmail.com>
2018-03-19 12:56:04 +0530

    Read from one file and write to another

--- FileReaderExample1.java
+++ FileReaderExample1.java
@@ -1,12 +1,19 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 class FileReaderExample1 {
         public static void main(String args[]){
         try {
-        FileReader f = new FileReader("test.txt");
-        char c = (char)f.read();
-        System.out.println(c);
-        f.close();
+        FileReader f1 = new FileReader("test.txt");
+	FileWriter f2 = new FileWriter("test2.txt");
+	int ch;
+	while((ch=f1.read())!=-1){
+		//c = (char)f.read();
+	//	System.out.println(ch);
+		f2.write((char)ch);
+	}
+	f1.close();
+	f2.close();
         }
         catch(IOException e) {
         System.out.println(e);
