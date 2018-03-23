b14c155cdf5b133eac7c4669f3a4dc8c0a86ac02
ruchika1706 <luthraruchika19@gmail.com>
2018-03-23 14:51:18 +0530

    Addition of way to run the program

--- AssertionErrorTest.java
+++ AssertionErrorTest.java
@@ -1,6 +1,7 @@
 class AssertionErrorTest {
     public static void main(String args[]){
         int x=5;
-        assert x>10:"X is greater than 10";
+     	// Compile and then run the program using java -ea AssertionErrorTest
+	   assert x>10:"X is greater than 10";
     }
 }
