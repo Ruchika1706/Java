d96999e32d53b8d42199313fc903451299034830
ruchika1706 <luthraruchika19@gmail.com>
2018-04-04 18:13:13 +0530

    Generate a random 10 character string of uppercase letters

--- RandomNumber.java
+++ RandomNumber.java
@@ -2,7 +2,8 @@ import java.util.Random;
 class RandomNumber {
     public static void main(String args[]){
         Random ob = new Random();
-        for(int i=1;i<=10;i++){
+        /* Print a 10 character random string using A to Z */
+	for(int i=1;i<=10;i++){
             int c = ob.nextInt(25)+65;
             System.out.print((char)c);
         }
