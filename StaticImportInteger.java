094fb6cc52ae7fc398e240601ead07055a6fc796
ruchika1706 <luthraruchika19@gmail.com>
2018-04-18 09:47:58 +0530

    Integer class static variable import using static import

---
+++ StaticImportInteger.java
@@ -0,0 +1,8 @@
+import static java.lang.Integer.*;
+class StaticImportInteger {
+    public static void main(String args[]) {
+        System.out.println(MAX_VALUE);
+        System.out.println(MIN_VALUE);
+        System.out.println(SIZE);
+    }
+}
